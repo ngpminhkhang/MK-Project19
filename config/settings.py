f653750b61c05b9e2f35f9b86ae1383f16ba8aa1
ngpminhkhang <ngpminhkhang@gmail.com>
2025-10-18 20:22:56 +0700

    Fix S3 env vars

--- config/settings.py
+++ config/settings.py
@@ -184,10 +184,10 @@ if 'AWS_STORAGE_BUCKET_NAME' in os.environ:
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-    AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'ap-southeast-1')  # Default region
+    AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'ap-southeast-2')  # Default Sydney region để fallback
     AWS_S3_FILE_OVERWRITE = False  # Không overwrite file cũ
     AWS_DEFAULT_ACL = 'public-read'  # File public
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'  # URL media từ S3
 else:
     print("Using local storage fallback")  # Debug: Nếu env vars sai
