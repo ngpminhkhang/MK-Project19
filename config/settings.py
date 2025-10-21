e40038aec1d72850ee523b713cef6f91be6b6973
ngpminhkhang <ngpminhkhang@gmail.com>
2025-10-21 23:13:54 +0700

    Fix INSTALLED_APPS missing finance_dashboard

--- config/settings.py
+++ config/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'finance_dashboard',
+
     # third-party
     'storages',
     # your apps
